eeba0f2102a285e100a4de257647c292c7f4ce7e
Chinedum Ukejianya <cukejianya@gmail.com>
2019-11-11 22:38:25 -0500

    Add a search screen

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import LoginScreen from '../screens/LoginScreen'
+import SearchScreen from '../screens/SearchScreen'
 import MainTabNavigator from './MainTabNavigator';
 
 export default createAppContainer(
   createSwitchNavigator({
     App: MainTabNavigator,
     Auth: LoginScreen,
+    Search: SearchScreen,
   }, {
-    initialRouteName: 'Auth',
+    initialRouteName: 'Search',
   })
 );
---
+++ screens/SearchScreen.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import {
+  Button,
+  StyleSheet,
+  TextInput,
+  View,
+} from 'react-native';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+
+import { MonoText } from '../components/StyledText';
+
+const SEARCH = gql`
+  query SearchPlace($lat: Float, $lng: Float, $title: String) {
+    searchPlace(lat: $lat, lng: $lng, title: $title) {
+      title
+      vicinity
+      category
+    }
+  }
+`;
+
+export default function SearchScreen() {
+  const [search, setSearch] = useState(0);
+  let lat = 39.33136;
+  let lng = -76.63226;
+
+  
+  var variables = { title: search, lat, lng} 
+  const {loading, error, data} = useQuery(SEARCH, { variables })
+  console.log(variables, data)
+
+  //if (error) {
+  //  console.log(error, search)
+  //}
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.loginInput}
+        autoCapitalize='none'
+        placeholder='Pick A Lunch Spot'
+        onChangeText={setSearch}
+       />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  loginInput: {
+    height: 40,
+    marginLeft: 40,
+    marginRight: 40,
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderColor: 'gray',
+    borderWidth: 1,
+    fontSize: 20,
+  }
+});
