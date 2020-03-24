4d0a6aa0a0a5785456c8f7f0d2964b9a18a995ec
Chinedum Ukejianya <cukejianya@gmail.com>
2020-03-24 05:51:26 -0400

    Add contact screen

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import ContactScreen from '../screens/ContactScreen'
 import LoginScreen from '../screens/LoginScreen'
 import SearchScreen from '../screens/SearchScreen'
 import MapScreen from '../screens/MapScreen'
@@ -12,11 +13,12 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Search"
+        initialRouteName="Contact"
       >
         <Stack.Screen name="Auth" component={LoginScreen} />
-        <Stack.Screen name="Search" component={SearchScreen} />
+        <Stack.Screen name='Contact' component={ContactScreen} />
         <Stack.Screen name="Map" component={MapScreen} />
+        <Stack.Screen name="Search" component={SearchScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
---
+++ screens/ContactScreen.js
@@ -0,0 +1,140 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import {
+  FlatList,
+  Keyboard,
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
+import * as Contacts from 'expo-contacts';
+import * as Permissions from 'expo-permissions';
+import { Ionicons } from '@expo/vector-icons'
+
+function Item({ isSelected, item, onSelect }) {
+  const color = isSelected ? "green" : "black";
+  console.log(color);
+  return (
+    <TouchableOpacity
+      onPress={() => onSelect(item.id)}
+      style={ styles.itemContainer }>
+      <Ionicons style={styles.itemIcon} name="ios-contact" size={30} color={color} />
+      <View style={ styles.itemText }>
+        <Text style={styles.itemTitle}>
+          { item.name }
+        </Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+export default function ContactScreen() {
+  const [search, setSearch] = useState('');
+  const [contacts, setContacts] = useState([]);
+  const [filterContacts, setFilterContacts] = useState([]);
+  const [selected, setSelected] = useState(new Set([]));
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Permissions.askAsync(Permissions.CONTACTS);
+      if (status === 'granted') {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.FirstName, Contacts.Fields.PhoneNumbers],
+        });
+
+        let contactData = data;
+        setContacts(contactData);
+      }
+    })();
+  }, []);
+  
+  useEffect(() => {
+    if (search) {
+      setFilterContacts(contacts.filter((obj) => (obj.firstName || '')
+        .toLowerCase()
+        .startsWith(search.toLowerCase())
+      ));
+    } else {
+      setFilterContacts(contacts)
+    }
+  }, [search, contacts])
+
+  const onSelect = useCallback((id) => {
+    setSelected(new Set(selected.delete(id) ? selected : selected.add(id)));
+  }, [selected]);
+
+  console.log(selected)
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.loginInput}
+          autoCapitalize='none'
+          placeholder='Pick The Lunch Crew'
+          onChangeText={setSearch}
+          value={search}
+          onBlur={Keyboard.dismiss}
+        />
+      </View>
+      <FlatList
+        data={filterContacts}
+        renderItem={({ item }) => (
+          <Item
+            isSelected={selected.has(item.id)}
+            item={item} 
+            onSelect={onSelect}
+          />
+        )}
+        keyExtractor={item => item.id}
+        extraData={selected}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 20,
+  },
+  inputContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgrey',
+  },
+  loginInput: {
+    height: 40,
+    marginLeft: 40,
+    marginRight: 40,
+    marginBottom: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderColor: 'gray',
+    borderWidth: 1,
+    fontSize: 20,
+  },
+  itemContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 20,
+    marginRight: 40,
+  },
+  itemIcon: {
+    marginTop: 15,
+    paddingBottom: 15,
+  },
+  itemText: {
+    flex: 1,
+    flexDirection: 'column',
+    marginTop: 15,
+    marginLeft: 10,
+    paddingBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgrey',
+  },
+  itemTitle: {
+    fontSize: 20,
+  },
+})
